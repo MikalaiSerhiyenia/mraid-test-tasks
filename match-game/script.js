20ae513ff89e12ffadb380fd53f72ff15aed4fcf
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-01-24 17:28:09 +0300

    refactor: refactor code, improve code readability, split large functions into smaller ones (Wed, Jan 24, 2024 05:28:09 PM)

--- match-game/script.js
+++ match-game/script.js
@@ -5,52 +5,57 @@ document.addEventListener("DOMContentLoaded", () => {
   play();
 });
 
+document.addEventListener("click", (event) => {
+  handleClick(event);
+});
+
+// Set the countdown time and mark it as not started
+
+let seconds = 59;
+let countdownStarted = false;
+
 // Create a map with paths to images
 
 const images = ["banana", "coco", "grape", "lemon", "lime", "pear"];
 
-const image = randomImage(images);
+// Create elements
 
 const main = document.createElement("main");
 main.classList.add("main");
-document.body.appendChild(main);
+
+const matchGame = document.createElement("div");
+matchGame.classList.add("match-game");
+
+const gameInfo = document.createElement("div");
+gameInfo.classList.add("match-game__info");
+
+const gameTitle = document.createElement("div");
+gameTitle.classList.add("match-game__info__title");
+gameTitle.innerHTML = "<img src='./images/text.png' alt='Match 3 tiles'>";
+
+const gameTimer = document.createElement("div");
+gameTimer.classList.add("match-game__info__timer");
+gameTimer.innerHTML = "<img src='./images/timer.png' alt='Timer'>";
+
+const countdown = document.createElement("span");
+countdown.classList.add("match-game__info__countdown");
+countdown.textContent = `:59`;
+
+const gamePlayField = document.createElement("div");
+gamePlayField.classList.add("match-game__play-field");
+
+const gameRow = document.createElement("div");
+gameRow.classList.add("match-game__play-field__row");
+
+const gameItem = document.createElement("div");
+gameItem.classList.add("match-game__play-field__item");
+gameItem.innerHTML = `<img src='./images/box.png' alt='Game Item' class='box'>`;
+
+createPlayField(gamePlayField);
 
 // Create a function to start the game
 
 function startGame() {
-  const matchGame = document.createElement("div");
-  matchGame.classList.add("match-game");
-
-  const gameInfo = document.createElement("div");
-  gameInfo.classList.add("match-game__info");
-
-  const gameTitle = document.createElement("div");
-  gameTitle.classList.add("match-game__info__title");
-  gameTitle.innerHTML = "<img src='./images/text.png' alt='Match 3 tiles'>";
-
-  const gameTimer = document.createElement("div");
-  gameTimer.classList.add("match-game__info__timer");
-  gameTimer.innerHTML = "<img src='./images/timer.png' alt='Timer'>";
-
-  const countdown = document.createElement("span");
-  countdown.classList.add("match-game__info__countdown");
-  countdown.textContent = `:59`;
-
-  const gamePlayField = document.createElement("div");
-  gamePlayField.classList.add("match-game__play-field");
-
-  for (let i = 0; i < 8; i++) {
-    const gameRow = document.createElement("div");
-    gameRow.classList.add("match-game__play-field__row");
-    for (let j = 0; j < 8; j++) {
-      const gameItem = document.createElement("div");
-      gameItem.classList.add("match-game__play-field__item");
-      gameItem.innerHTML = `<img src='./images/box.png' alt='Game Item' class='box'>`;
-      gameRow.appendChild(gameItem);
-    }
-    gamePlayField.appendChild(gameRow);
-  }
-
   document.body.appendChild(main);
   main.appendChild(matchGame);
   matchGame.appendChild(gameInfo);
@@ -61,33 +66,14 @@ function startGame() {
 
   const items = document.querySelectorAll(".match-game__play-field__item");
 
-  fillGameField(items, image);
-}
-
-// Create a function for selecting a random image
-
-function randomImage(array) {
-  const item = Math.floor(Math.random() * array.length);
-  return array[item];
-}
-
-// Create a function to fill a field with random images
-
-function fillGameField(element, array) {
-  element.forEach((i) => {
-    const item = randomImage(images);
-    i.innerHTML += `<img src='./images/${item}.png' alt='${item}' class='item ${item}'>`;
-  });
+  fillGameField(items);
 }
 
 // Process clicks on field elements
 
-document.addEventListener("click", handleClick);
-
-let countdownStarted = false;
-
 function handleClick(event) {
   const target = event.target;
+
   if (target.classList.contains("item")) {
     if (!countdownStarted) {
       startCountdown();
@@ -97,41 +83,16 @@ function handleClick(event) {
   }
 }
 
-// Create countdown function from 30 to 0
-
-function startCountdown() {
-  const countdown = document.querySelector(".match-game__info__countdown");
-  let seconds = 59;
-  function updateCountdown() {
-    if (seconds >= 10) {
-      countdown.textContent = `:${seconds}`;
-      seconds--;
-      setTimeout(updateCountdown, 1000);
-    } else if (seconds >= 0 && seconds < 10) {
-      countdown.textContent = `:0${seconds}`;
-      seconds--;
-      setTimeout(updateCountdown, 1000);
-    } else {
-      console.log("Время вышло!");
-      endGame();
-    }
-  }
-
-  updateCountdown();
-}
-
 // Play function
 
 function play() {
   const playField = document.querySelector(".match-game__play-field");
   let selectedTiles = [];
 
-  playField.removeEventListener("click", handleTileClick);
   playField.addEventListener("click", handleTileClick);
 
   function handleTileClick(event) {
     const target = event.target.closest(".match-game__play-field__item");
-
     if (
       !target ||
       target.classList.contains("matched") ||
@@ -140,40 +101,32 @@ function play() {
       return;
     }
 
-    target.classList.add("active");
-    selectedTiles.push(target);
+    addClassList(target, selectedTiles);
 
-    let altValues = selectedTiles.map(
-      (tile) => tile.querySelector(".item").alt
-    );
+    const altValues = getAltValues(selectedTiles);
+    const className = altValues[0];
 
     if (selectedTiles.length > 1) {
       let areAllAltValuesEqual = altValues.every(
-        (value, index, array) => value === array[0]
+        (value) => value === altValues[0]
       );
 
       if (!areAllAltValuesEqual) {
         selectedTiles.forEach((tile) => tile.classList.add("incorrect"));
         setTimeout(() => {
-          selectedTiles.forEach((tile) => {
-            tile.classList.remove("active", "incorrect");
-          });
+          selectedTiles.forEach((tile) =>
+            tile.classList.remove("active", "incorrect")
+          );
           selectedTiles = [];
-        }, 1000);
+        }, getAnimationDuration());
         return;
       }
 
-      let countElementsWithClass = document.querySelectorAll(
-        `.item.${altValues[0]}`
-      ).length;
-
-      if (selectedTiles.length === countElementsWithClass) {
-        selectedTiles.forEach((tile) => {
-          tile.classList.add("matched");
-        });
+      if (selectedTiles.length === countElementsWithClass(className)) {
+        selectedTiles.forEach((tile) => tile.classList.add("matched"));
         setTimeout(() => {
           selectedTiles = [];
-        }, 1000);
+        }, getAnimationDuration());
       }
     }
   }
@@ -191,7 +144,7 @@ function endGame() {
   setTimeout(() => {
     main.innerHTML = "";
     restart();
-  }, 1000);
+  }, getAnimationDuration());
 }
 
 // Restart game function
@@ -203,7 +156,8 @@ function restart() {
 
   const gameResultMessage = document.createElement("div");
   gameResultMessage.classList.add("game-result__message");
-  gameResultMessage.textContent = "Game Over";
+
+  checkResult(gameResultMessage);
 
   const playButton = document.createElement("button");
   playButton.type = "button";
@@ -226,4 +180,93 @@ function restart() {
 
   playButton.removeEventListener("click", restartGame);
   playButton.addEventListener("click", restartGame);
+}
+
+// Getting alt values
+
+function getAltValues(array) {
+  return array.map((tile) => tile.querySelector(".item").alt);
+}
+
+// Adding class
+
+function addClassList(target, array) {
+  target.classList.add("active");
+  array.push(target);
+}
+
+// Count elems with class
+
+function countElementsWithClass(classname) {
+  return document.querySelectorAll(`.item.${classname}`).length;
+}
+
+// Update countdown
+
+function updateCountdown(sec, elem) {
+  if (sec >= 10) {
+    elem.textContent = `:${sec}`;
+  } else if (sec >= 0 && sec < 10) {
+    elem.textContent = `:0${sec}`;
+  } else {
+    endGame();
+    return;
+  }
+
+  sec--;
+  setTimeout(() => updateCountdown(sec, elem), getAnimationDuration());
+}
+
+// Create countdown function from 30 to 0
+
+function startCountdown() {
+  const countdownElem = document.querySelector(".match-game__info__countdown");
+  updateCountdown(seconds, countdownElem);
+}
+
+// check result
+
+function checkResult(elem) {
+  const matchedLength = document.querySelectorAll(".matched").length;
+  return matchedLength === 64
+    ? (elem.textContent = "You win!")
+    : (elem.textContent = "Game Over");
+}
+
+// Create play field
+
+function createPlayField(elem1) {
+  for (let i = 0; i < 8; i++) {
+    const gameRow = document.createElement("div");
+    gameRow.classList.add("match-game__play-field__row");
+    for (let j = 0; j < 8; j++) {
+      const gameItem = document.createElement("div");
+      gameItem.classList.add("match-game__play-field__item");
+      gameItem.innerHTML = `<img src='./images/box.png' alt='Game Item' class='box'>`;
+      gameRow.appendChild(gameItem);
+    }
+    elem1.appendChild(gameRow);
+  }
+}
+
+// Create a function for selecting a random image
+
+function randomImage(array) {
+  const item = Math.floor(Math.random() * array.length);
+  return array[item];
+}
+
+// Create a function to fill a field with random images
+
+function fillGameField(array) {
+  array.forEach((i) => {
+    const image = randomImage(images);
+    i.innerHTML += `<img src='./images/${image}.png' alt='${image}' class='item ${image}'>`;
+  });
+}
+
+// Get animation duration
+
+function getAnimationDuration() {
+  return 1000;
 }
